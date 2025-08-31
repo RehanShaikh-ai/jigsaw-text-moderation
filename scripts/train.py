359ae534c6efdd35f6c8c874e9045e60221fbcef
RehanShaikh-ai <rehansk.3107@gmail.com>
2025-08-31 17:11:28 +0530

    add model training and configs

--- scripts/train.py
+++ scripts/train.py
@@ -1,4 +1,6 @@
 from utils import preprocess, etl, features
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
 
 import pandas as pd
 
@@ -10,3 +12,7 @@ print(pd.concat([X,y], axis=1))
 X_train,X_val, y_train, y_val= etl.split_data(X,y)
 X_train, X_val, tfidf_model =features.get_features(X_train, X_val) 
 
+log_reg = LogisticRegression(max_iter=200, solver='liblinear')
+model = OneVsRestClassifier(log_reg)
+
+model.fit(X_train, y_train)
