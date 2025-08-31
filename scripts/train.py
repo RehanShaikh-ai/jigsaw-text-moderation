cca99cd99f3e5f433bbc9e577994165d1f5826d1
RehanShaikh-ai <rehansk.3107@gmail.com>
2025-08-31 14:43:07 +0530

    add features.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,13 +1,12 @@
-from utils import preprocess, etl 
+from utils import preprocess, etl, features
 
 import pandas as pd
+
 df = etl.extract_data("data/raw/train.csv")
 X, y = preprocess.preprocess(df)
 print("DataFrame:")
 print(pd.concat([X,y], axis=1))
 
 X_train,X_val, y_train, y_val= etl.split_data(X,y)
-print("Train DataFrame:")
-print(X_train)
-print("\nValidation DataFrame:")
-print(X_val)
+X_train, X_val, tfidf_model =features.get_features(X_train, X_val) 
+
---
+++ scripts/utils/features.py
@@ -0,0 +1,9 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def get_features(train, val, max_features = 5000):
+    vectorizer = TfidfVectorizer(max_features=max_features)
+    X_train = vectorizer.fit_transform(train)
+    X_val = vectorizer.fit_transform(val)
+
+    return X_train, X_val, vectorizer
+
