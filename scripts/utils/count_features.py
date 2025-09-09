ad19c393b388c9592b165f16a659adfca561817b
RehanShaikh-ai <rehansk.3107@gmail.com>
2025-09-09 19:50:22 +0530

    complete NB log-count ratio for one label

---
+++ scripts/utils/count_features.py
@@ -0,0 +1,20 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
+def get_count_features(X, y):
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(X)
+    alpha = 1
+    for i in y.columns:     
+        pos = X[y[i]==1].sum(axis=0).flatten() + alpha
+        neg = X[y[i]==0].sum(axis=0).flatten() + alpha
+        
+        total_pos = pos.sum()
+        total_neg = neg.sum()
+
+        P_w_pos = pos / total_pos
+        P_w_neg = neg / total_neg
+
+        r = np.log(P_w_pos/P_w_neg)
+        
+        
